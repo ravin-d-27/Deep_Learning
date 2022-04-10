b32ef074e854d668fc6add55d98d47cf6c5be753
Ravin D <ravin.d3104@outlook.com>
2022-04-10 13:42:01 +0530

    I have created a OOP file for CNN

---
+++ Convolutional_Neural_Networks/CNN_OOPS.py
@@ -0,0 +1,29 @@
+
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+class CNN_DL:
+
+    def __init__():
+        pass
+
+    def train_data_preprocess():
+        pass
+
+    def test_data_preprocess():
+        pass
+
+    def CNN():
+        pass
+
+    def train():
+        pass
+
+    def predict():
+        pass
+
+
+
